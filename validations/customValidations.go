ea8ba44dd08820ac52adedf85713768bdba340c6
Gustavo Lima <gustavolimash@gmail.com>
2022-07-26 15:30:30 -0300

    last version of cpf validation method

--- validations/customValidations.go
+++ validations/customValidations.go
@@ -1,7 +1,6 @@
 package validations
 
 import (
-	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
@@ -12,39 +11,43 @@ import (
 func CPFValidation(field validator.FieldLevel) bool {
 	cpf := field.Field().Interface().(string)
 
-	soma := 0
+	sum := 0
 	aux := 0
 
 	noDots := strings.Replace(cpf, ".", "", -1)
 	trimCPF := strings.Replace(noDots, "-", "", -1)
 
-	fmt.Println(trimCPF)
-
 	if trimCPF == "11111111111" || trimCPF == "22222222222" {
-		fmt.Println("1")
 		return false
 	}
 
-	match, _ := regexp.MatchString(`([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})`, trimCPF)
-	if match != true {
+	match, err := regexp.MatchString(`([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})`, trimCPF)
+	if err != nil {
+		return false
+	}
+
+	if !match {
 		return false
 	}
 
 	numbers := []rune(trimCPF)
 	for i := 1; i <= 9; i++ {
-		aux, _ = strconv.Atoi(string(numbers[i-1]))
-		soma += aux * (11 - i)
+		aux, err = strconv.Atoi(string(numbers[i-1]))
+		if err != nil {
+			return false
+		}
+		sum += aux * (11 - i)
 	}
 
-	resto := (soma * 10) % 11
-	if resto == 10 || resto == 11 {
-		resto = 0
+	mod := (sum * 10) % 11
+	if mod == 10 || mod == 11 {
+		mod = 0
 	}
 
-	aux, _ = strconv.Atoi(string(numbers[9]))
-	if resto != aux {
+	aux, err = strconv.Atoi(string(numbers[9]))
+	if err != nil {
 		return false
 	}
 
-	return true
+	return aux == mod
 }
